d1452d1bec448cbdb41d0d0e1c2e2b2d39bd06c5
Nebi Sarikaya <nebsar@gmail.com>
2020-02-04 01:41:30 -0500

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Nebi SARIKAYA on 2020-02-04.
+//  Copyright © 2020 Nebi SARIKAYA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
